ab83b9beefb836f8ddc74ae44864f4b9d804dbb9
musamaa9 <muhammad.usama@outthink.io>
2024-05-25 02:48:01 +0100

    basic Swagger and open api settings added

--- ToDoApp.Api/Program.cs
+++ ToDoApp.Api/Program.cs
@@ -1,7 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!")
+    .WithOpenApi()
+    .Produces(200, typeof(string), "application/plain");
 
 app.Run();
